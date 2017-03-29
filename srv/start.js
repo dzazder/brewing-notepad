97d17881de43461b418868036560aff8f92de940
Bartosz Lampart <dzazder@gmail.com>
2017-03-29 18:54:54 +0200

    first commit

---
+++ srv/start.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var path = require('path');
+var open = require('open');
+
+var port = 3000;
+var app = express();
+
+//prepare server
+app.use("/js", express.static(path.join(__dirname, "../node_modules/bootstrap/dist/js")));
+app.use("/js", express.static(path.join(__dirname, "../node_modules/jquery/dist")));
+app.use("/content", express.static(path.join(__dirname, "../node_modules/bootstrap/dist/css")));
+app.use("/templates", express.static(path.join(__dirname, "../templates")));
+
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.listen(port, function(err) {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        open('http://localhost:' + port);
+    }
+})
