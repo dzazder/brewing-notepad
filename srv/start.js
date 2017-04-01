b3ae8940c620f0bdad922043848b29de65136eb5
Bartosz Lampart <dzazder@gmail.com>
2017-04-01 16:03:50 +0200

    postgres

--- srv/start.js
+++ srv/start.js
@@ -2,6 +2,24 @@ var express = require('express');
 var path = require('path');
 var open = require('open');
 
+const pg = require('pg')  
+const conString = 'postgres://postgres:rzWLKE4L@localhost/bn' // make sure to match your own database's credentials
+
+pg.connect(conString, function (err, client, done) {  
+  if (err) {
+    return console.error('error fetching client from pool', err);
+  }
+  client.query('SELECT $1::varchar AS my_first_query', ['node hero'], function (err, result) {
+    done();
+
+    if (err) {
+      return console.error('error happened during query', err);
+    }
+    console.log(result.rows[0]);
+    //process.exit(0)
+  })
+})
+
 var port = 3000;
 var app = express();
 
